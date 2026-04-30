5c031f4888d064f6aca862fb39271870fb26875d
Anilsonx <anilsonx@gmail.com>
2026-04-30 17:36:12 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,27 @@
+const config = {
+  empresa: "Panorama VR",
+  telefone: "5594991766944",
+
+  titulo: "Apareça no topo do Google",
+  subtitulo: "Mais clientes todos os dias",
+
+  descricao: "Serviços para posicionar empresas no Google",
+
+  problema: "Sua empresa não aparece no Google?",
+
+  prova: "Empresas bem posicionadas recebem mais clientes.",
+
+  servicos: [
+    {titulo: "Otimização Google", descricao: "Mais visibilidade"},
+    {titulo: "Tour Virtual", descricao: "Mais impacto"},
+    {titulo: "Drone", descricao: "Mais profissionalismo"}
+  ],
+
+  reviews: [
+    "Excelente serviço!",
+    "Aumentei meus clientes!",
+    "Muito profissional!"
+  ],
+
+  mapa: "https://www.google.com/maps?q=Pará&output=embed"
+};
